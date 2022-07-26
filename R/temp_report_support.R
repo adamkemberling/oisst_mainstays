474be30b844456860ac5dfe49353139d0da71f43
Adam <adamkemberling@gmail.com>
2022-07-26 13:54:59 -0400

    Changed map to include color versions and contour overlays

--- R/temp_report_support.R
+++ R/temp_report_support.R
@@ -584,6 +584,99 @@ get_masked_vals <- function(masked_ranks, masked_rates, in_fahrenheit = F){
 
 
 
+
+# Convert a raster to dataframe
+rasterdf <- function(x, aggregate = 1) {
+  
+  # FROM: https://bookdown.org/mcwimberly/gdswr-book/raster-data-discrete-variables.html
+  resampleFactor <- aggregate
+  inputRaster <- x
+  inCols <- ncol(inputRaster)
+  inRows <- nrow(inputRaster)
+  # Compute numbers of columns and rows in the new raster for mapping
+  resampledRaster <- raster(ncol=(inCols / resampleFactor),
+                            nrow=(inRows / resampleFactor))
+  # Match to the extent of the original raster
+  extent(resampledRaster) <- extent(inputRaster)
+  # Resample data on the new raster
+  y <- resample(inputRaster,resampledRaster,method='ngb')
+  
+  # Extract cell coordinates
+  coords <- xyFromCell(y, seq_len(ncell(y)))
+  dat <- stack(as.data.frame(getValues(y)))
+  
+  # Add names - 'value' for data, 'variable' to indicate different raster layers
+  # in a stack
+  names(dat) <- c('value', 'variable')
+  dat <- cbind(coords, dat)
+  dat
+}
+
+
+
+
+
+#' @title Reclassify Bathymetry Raster in Set Increments
+#'
+#' @param bathy_ras 
+#' @param depth_increments 
+#' @param min_elev 
+#' @param max_elev 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+reclassify_bathy <- function(bathy_ras = NULL, 
+                             depth_increments = -200,
+                             min_elev = -1000,
+                             max_elev = 200,
+                             include_lower = FALSE){
+  
+  # 1. create classification matrix
+  # Gets converted to matrix: pattern is [left_lim:right_lim:label]
+  
+  # Get the left and right limits using increments
+  l_lims <- seq(from = max_elev + depth_increments, to = min_elev, by = depth_increments)
+  r_lims <- seq(from = max_elev, to = (min_elev - depth_increments), by = depth_increments)
+  
+  # Get the factor order that go with them (need numeric valus b/c matrix)
+  lnums  <- as.numeric(fct_rev(as.factor(l_lims))) 
+  
+  # Build text labels as a key for viz later
+  llabs  <- map2_chr(l_lims, r_lims, ~ str_c(.y, " to ", .x)) 
+  
+  # Make greater than if including lower than limit
+  if(include_lower){
+    llabs[length(llabs)] <- str_c("Greater than ", min_elev)  
+  }
+  
+  # Build the dataframe that becomes matrix
+  reclass_df <- data.frame("left" = l_lims, "right" = r_lims, "bin_num" = lnums)
+  
+  # reshape the df into a matrix with columns and rows
+  reclass_m <- as.matrix(reclass_df)
+  
+  # Add labels to the df for exporting
+  reclass_df$bin_labs <- llabs
+  reclass_df <- mutate(reclass_df, bin_labs = fct_reorder(bin_labs, bin_num, min))
+  
+  # Truncate to limits of the range we want
+  bathy_ras <- clamp(bathy_ras, min_elev, max_elev, useValues = TRUE)
+  
+  # 2. reclassify the raster using the reclass object - reclass_m
+  bathy_classified <- reclassify(bathy_ras,
+                                 reclass_m, 
+                                 include.lowest = include_lower)
+  
+  # Return Both the Raster and its key
+  return(
+    list(
+      "ras" = bathy_classified,
+      "labs" = reclass_df))
+}
+
+
 ####____####
 
 ####  Specific/Standard  Plots  ####
@@ -592,12 +685,29 @@ get_masked_vals <- function(masked_ranks, masked_rates, in_fahrenheit = F){
 
 
 # Function to map  Gulf of Maine, with region of interest overlaid
+#' @title Minimal Map of Study area
+#'
+#' @param region_extent 
+#' @param x_buffer 
+#' @param y_buffer 
+#' @param new_england_sf 
+#' @param canada_sf 
+#' @param greenland_sf 
+#' @param area_labs 
+#' @param plot_title 
+#' @param shape_linetype 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 map_study_area <- function(region_extent,
                            x_buffer =  c(-2.25, 2.25),
                            y_buffer = c(-0.75, 0.75),
                            new_england_sf = NULL,
                            canada_sf = NULL,
                            greenland_sf = NULL,
+                           area_labs = NULL,
                            plot_title = NULL,
                            shape_linetype = 2){
   
@@ -611,6 +721,34 @@ map_study_area <- function(region_extent,
   }
   if(is.null(greenland_sf)){
     greenland_sf   <- ne_states(country = "greenland", returnclass = "sf")
+  }
+  if(is.null(area_labs)){
+    area_labs<- data.frame(
+      "label" = c("Georges\nBank", 
+                  "Jordan\nBasin", 
+                  "Wilkinson\nBasin", 
+                  "Northeast\nChannel",
+                  "Scotian\nShelf",
+                  "Mid-Atlantic\nBight"),
+      "lat"   = c(41.3, 
+                  43.5, 
+                  42.275, 
+                  42.25,
+                  43.7,
+                  39.8),
+      "lon"   = c(-67.7, 
+                  -67.5, 
+                  -69.2, 
+                  -66.1,
+                  -63.5,
+                  -72.8),
+      "angle" = c(0, 
+                  0, 
+                  0, 
+                  0,
+                  0,
+                  0)
+    )
   }
   
   
@@ -644,6 +782,8 @@ map_study_area <- function(region_extent,
     geom_sf(data = region_extent, 
             color = gmri_cols("gmri blue"), 
             fill = gmri_cols("gmri blue"), alpha = 0.2, linetype = shape_linetype, size = 0.5) +
+    geom_text(data = area_labs, aes(lon, lat, label = label, angle = angle), 
+              size = 3, color = "black") +
     coord_sf(xlim = crop_x, 
              ylim = crop_y, expand = T) +
     map_theme() +
@@ -652,6 +792,147 @@ map_study_area <- function(region_extent,
   
   # Show figure
   return(gom_extent_p)
+  
+}
+
+
+
+
+
+
+#' @title Colored Map of Study area
+#'
+#' @param region_extent 
+#' @param x_buffer 
+#' @param y_buffer 
+#' @param new_england_sf 
+#' @param canada_sf 
+#' @param greenland_sf 
+#' @param plot_title 
+#' @param area_labs 
+#' @param shape_linetype 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+map_study_area_color <- function(region_extent,
+                                 x_buffer =  c(-2.25, 2.25),
+                                 y_buffer = c(-0.75, 0.75),
+                                 new_england_sf = NULL,
+                                 canada_sf = NULL,
+                                 greenland_sf = NULL,
+                                 plot_title = NULL,
+                                 area_labs = NULL,
+                                 shape_linetype = 2){
+  
+  
+  
+    
+    
+    # Load country borders if NULL
+    if(is.null(new_england_sf)){
+      new_england_sf <- ne_states("united states of america", returnclass = "sf")
+    }
+    if(is.null(canada_sf)){
+      canada_sf      <- ne_states("canada", returnclass = "sf")
+    }
+    if(is.null(greenland_sf)){
+      greenland_sf   <- ne_states(country = "greenland", returnclass = "sf")
+    }
+    if(is.null(area_labs)){
+      area_labs<- data.frame(
+        "label" = c("Georges\nBank", 
+                    "Jordan\nBasin", 
+                    "Wilkinson\nBasin", 
+                    "Northeast\nChannel",
+                    "Scotian\nShelf",
+                    "Mid-Atlantic\nBight"),
+        "lat"   = c(41.3, 
+                    43.5, 
+                    42.275, 
+                    42.25,
+                    43.7,
+                    39.8),
+        "lon"   = c(-67.7, 
+                    -67.5, 
+                    -69.2, 
+                    -66.1,
+                    -63.5,
+                    -72.8),
+        "angle" = c(0, 
+                    0, 
+                    0, 
+                    0, #315,
+                    0,
+                    0)
+      )
+    }
+    
+    
+    # Pull extents for the region for crop
+    crop_x <- st_bbox(region_extent)[c(1,3)]
+    crop_y <- st_bbox(region_extent)[c(2,4)]
+    
+    # Expand the area out to see the larger patterns
+    crop_x <- crop_x + x_buffer
+    crop_y <- crop_y + y_buffer
+    
+    #
+    # Add the bottom contours:
+    bathy <- raster("~/Documents/Repositories/Points_and_contours/NEShelf_Etopo1_bathy.tiff")
+    
+    # # Contours for geom_contour()
+    # bathy_df <- as.data.frame(raster::coordinates(bathy))
+    # bathy_df$depth <- raster::extract(bathy, bathy_df)
+    # bathy_df$depth <- bathy_df$depth * -1
+    # contours_make <- c(50, 100, 250)
+    
+    # Add the bottom contours as color
+    # Reclassify to discrete
+    bathy_reclass <- reclassify_bathy(bathy, depth_increments = -100, min_elev = -600, max_elev = 0)
+    reclass_ras <- bathy_reclass$ras
+    reclass_labs  <- bathy_reclass$labs
+    
+    
+    # Convert to DF, add labels from reclassification ey
+    bathy_rclass_df <- rasterdf(reclass_ras) %>% 
+      select(x, y, bin_num = value) %>% 
+      left_join(reclass_labs, by = "bin_num")
+    
+    
+    
+    # Full map of GOM
+    gom_extent_p <- ggplot() +
+      geom_raster(data = bathy_rclass_df, aes(x,y, fill = bin_labs), alpha = 0.9) +
+      scale_fill_brewer(palette = "Blues", 
+                        # To remove NA's from legend:
+                        # na.translate = FALSE, 
+                        # To set their color for NA
+                        # Get fill from brewer: RColorBrewer::brewer.pal(n = 6, "Blues")[6]
+                        na.value = "#08519C",
+                        labels = c(levels(bathy_rclass_df$bin_labs), "Greater than -600"),
+                        name = "Depth")  +
+      geom_sf(data = new_england, fill = "gray90", size = .25) +
+      geom_sf(data = canada, fill = "gray90", size = .25) +
+      geom_sf(data = greenland, fill = "gray90", size = .25) +
+      # geom_contour(data = bathy_df, aes(x, y, z = depth),
+      #              breaks = contours_make,
+      #              color = "gray80") +
+      geom_text(data = area_labs, aes(lon, lat, label = label, angle = angle), 
+                size = 3, color = "black") +
+      geom_sf(data = region_extent, 
+              color = "gray10", 
+              fill = "transparent", alpha = 0.2, linetype = shape_linetype, size = 0.5) +
+      coord_sf(xlim = crop_x, 
+               ylim = crop_y, expand = T) +
+      map_theme(legend.position = "none") +
+      labs(title = plot_title,
+           caption = "Depth contours colored at 100m intervals to a maximum of 600m")
+    
+    
+    # Show figure
+    return(gom_extent_p)
   
 }
 
