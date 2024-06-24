949e2bc5ff98a6c0cf8abad6de88f497aacbcb59
adamkemberling <adamkemberling@gmail.com>
2024-06-24 13:00:11 -0400

    oops

---
+++ largefile_filter.R
@@ -0,0 +1,4 @@
+# https://www.deployhq.com/git/faqs/removing-large-files-from-git-history
+git filter-branch --force --index-filter \
+'git rm --cached --ignore-unmatch R/markdown_reports/Openspaces_data/Openspaces_rgb/sst2010sAverage_coldcolors.tif' \
+--prune-empty --tag-name-filter cat -- --all
